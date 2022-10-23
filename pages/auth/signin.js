224db3315b896da415024c8e95e63b0b0e34044d
unknown <erik17242@gmail.com>
2022-10-23 15:51:42 +0300

    finished adding auth

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,23 +1,29 @@
 import Header from "../../components/Header";
-import { getProviders } from "next-auth/react";
+import { getProviders, signIn } from "next-auth/react";
 
-function signIn({ providers }) {
+function signin({ providers }) {
   return (
     <div>
       <Header />
-      <div className="">
+      <div className="mt-40">
         {Object.values(providers).map((provider) => (
-          <div key={provider.name}>
+          <div key={provider.name} className="flex flex-col items-center">
             <img
+              className="w-52 object-cover"
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/2560px-Google_2015_logo.svg.png"
               alt="Logo"
               width={300}
               height={150}
             />
-            <p>
+            <p className="text-sm italic my-10 text-center">
               This website is created for learning purposes. I do not claim ownership of google!!
             </p>
-            <button>Sign in with {provider.name}</button>
+            <button
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+              className="bg-red-400 rounded-lg text-white p-3 hover:bg-red-500"
+            >
+              Sign in with {provider.name}
+            </button>
           </div>
         ))}
       </div>
@@ -34,4 +40,4 @@ export async function getServerSideProps() {
   };
 }
 
-export default signIn;
+export default signin;
